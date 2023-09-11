1d89166089c375408c201515026af82da2df86c0
emmadev <macanthonyemmanuel12@gmail.com>
2023-09-11 21:11:37 +0100

    'basics'

--- App.tsx
+++ App.tsx
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+  withSpring,
+  withRepeat,
+} from "react-native-reanimated";
+
+const SIZE = 100.0;
 
 export default function App() {
+  const progress = useSharedValue(1);
+  const scale = useSharedValue(2);
+
+  useEffect(() => {
+    progress.value = withRepeat(withSpring(0.5), 3, true);
+    scale.value = withRepeat(withSpring(1), 3, true);
+  }, []);
+
+  const handleRotate = (progress: Animated.SharedValue<number>) => {
+    "worklet";
+    return `${progress.value * 2 * Math.PI}rad`;
+  };
+
+  const reanimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value,
+      transform: [{ scale: scale.value }, { rotate: handleRotate(progress) }],
+      borderRadius: (progress.value * SIZE) / 2,
+    };
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View
+        style={[
+          {
+            height: SIZE,
+            width: SIZE,
+            backgroundColor: "blue",
+          },
+          reanimatedStyle,
+        ]}
+      />
     </View>
   );
 }
@@ -13,8 +51,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
