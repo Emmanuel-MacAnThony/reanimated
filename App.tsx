89bf8db9c5055fe67cf99b43cd008090e83fed66
emmadev <macanthonyemmanuel12@gmail.com>
2023-09-12 21:26:29 +0100

    implemented scroll view animation with interpolation

--- App.tsx
+++ App.tsx
@@ -8,90 +8,49 @@ import Animated, {
   withSpring,
   withRepeat,
   useAnimatedGestureHandler,
+  useAnimatedScrollHandler,
 } from "react-native-reanimated";
 import {
   GestureHandlerRootView,
   PanGestureHandler,
   PanGestureHandlerGestureEvent,
 } from "react-native-gesture-handler";
+import { Page } from "./components/Page";
 
-const SIZE = 80.0;
-const CIRCLE_RADIUS = SIZE * 2;
+const WORDS = ["What's", "up", "mobile", "devs?"];
 
 export default function App() {
   const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
 
-  const reanimatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: translateX.value },
-        { translateY: translateY.value },
-      ],
-    };
-  }, []);
-
-  type ContextType = {
-    translateX: number;
-    translateY: number;
-  };
-
-  const panGestureEvent = useAnimatedGestureHandler<
-    PanGestureHandlerGestureEvent,
-    ContextType
-  >({
-    onStart: (event, context) => {
-      context.translateX = translateX.value;
-      context.translateY = translateY.value;
-    },
-    onActive: (event, context) => {
-      translateX.value = event.translationX + context.translateX;
-      translateY.value = event.translationY + context.translateY;
-    },
-    onEnd: (event) => {
-      const distance = Math.sqrt(translateX.value ** 2 + translateY.value ** 2);
-      if (distance < CIRCLE_RADIUS + SIZE / 2) {
-        translateX.value = withSpring(0);
-        translateY.value = withSpring(0);
-      }
-    },
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    translateX.value = event.contentOffset.x;
   });
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <View style={styles.circle}>
-          <PanGestureHandler onGestureEvent={panGestureEvent}>
-            <Animated.View style={[styles.square, reanimatedStyle]} />
-          </PanGestureHandler>
-        </View>
-      </View>
-    </GestureHandlerRootView>
+    <Animated.ScrollView
+      horizontal
+      style={styles.container}
+      onScroll={scrollHandler}
+      scrollEventThrottle={16}
+      pagingEnabled
+    >
+      {WORDS.map((word, idx) => {
+        return (
+          <Page
+            key={idx.toString()}
+            index={idx}
+            title={word}
+            translateX={translateX}
+          />
+        );
+      })}
+    </Animated.ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  circle: {
-    width: CIRCLE_RADIUS * 2,
-    height: CIRCLE_RADIUS * 2,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: CIRCLE_RADIUS,
-    borderWidth: 5,
-    borderColor: "rgba(0, 0, 256, 0.5)",
-  },
-
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  square: {
-    width: SIZE,
-    height: SIZE,
-    backgroundColor: "rgba(0, 0, 256, 0.5)",
-    borderRadius: 20,
   },
 });
