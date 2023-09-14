be0e625ac11847d7a711676b6a7338812711854e
emmadev <macanthonyemmanuel12@gmail.com>
2023-09-14 17:25:28 +0100

    circular animation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler";
-import React, { useRef, useCallback } from "react";
+import React, { useRef, useCallback, useEffect } from "react";
 import {
   StyleSheet,
   Image,
@@ -8,6 +8,7 @@ import {
   View,
   Text,
   ImageBackground,
+  TouchableOpacity,
 } from "react-native";
 import {
   GestureHandlerRootView,
@@ -17,68 +18,98 @@ import {
 import Animated, {
   cancelAnimation,
   useAnimatedGestureHandler,
+  useAnimatedProps,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
   withDecay,
+  withTiming,
 } from "react-native-reanimated";
 import Page, { PAGE_WIDTH } from "./components/Page";
+import Svg, { Circle } from "react-native-svg";
+import { ReText } from "react-native-redash";
 
-const titles = ["whats", "up", "mobile", "devs"];
+const BACKGROUND_COLOR = "#444B6F";
+const BACKGROUND_STROKE_COLOR = "#303858";
+const STROKE_COLOR = "#A6E1FA";
+const { width, height } = Dimensions.get("window");
 
-type ContextType = {
-  x: number;
-};
+const CIRCLE_LENGTH = 1000;
+const R = CIRCLE_LENGTH / (2 * Math.PI);
+
+const AnimatedCIrcle = Animated.createAnimatedComponent(Circle);
 
 export default function App() {
-  const translateX = useSharedValue(0);
-  const clampedTranslateX = useDerivedValue(() => {
-    const MAX_TRANSLATE_X_VALUE = -PAGE_WIDTH * (titles.length - 1);
-    return Math.max(Math.min(translateX.value, 0), MAX_TRANSLATE_X_VALUE);
+  const progress = useSharedValue(0);
+  const progressText = useDerivedValue(() => {
+    return `${Math.floor(progress.value * 100)}`;
   });
 
-  const panGestureEvent = useAnimatedGestureHandler<
-    PanGestureHandlerGestureEvent,
-    ContextType
-  >({
-    onStart: (_, context) => {
-      context.x = clampedTranslateX.value;
-      cancelAnimation(translateX);
-    },
-    onActive: (event, context) => {
-      translateX.value = event.translationX + context.x;
-    },
-    onEnd: (event) => {
-      translateX.value = withDecay({ velocity: event.velocityX });
-    },
-  });
+  const animatedProps = useAnimatedProps(() => ({
+    strokeDashoffset: CIRCLE_LENGTH * (1 - progress.value),
+  }));
+
+  const onPress = useCallback(() => {
+    progress.value = withTiming(progress.value > 0 ? 0 : 1, { duration: 2000 });
+  }, []);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <StatusBar />
-        <PanGestureHandler onGestureEvent={panGestureEvent}>
-          <Animated.View style={{ flex: 1, flexDirection: "row" }}>
-            {titles.map((title, index) => {
-              return (
-                <Page
-                  key={index.toString()}
-                  index={index}
-                  title={title}
-                  translateX={clampedTranslateX}
-                />
-              );
-            })}
-          </Animated.View>
-        </PanGestureHandler>
-      </View>
-    </GestureHandlerRootView>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={BACKGROUND_COLOR} />
+
+      <ReText style={styles.progressText} text={progressText} />
+      <Svg style={{ position: "absolute" }} fill={"transparent"}>
+        <Circle
+          cx={width / 2}
+          cy={height / 2}
+          r={R}
+          stroke={BACKGROUND_STROKE_COLOR}
+          strokeWidth={30}
+        />
+        <AnimatedCIrcle
+          cx={width / 2}
+          cy={height / 2}
+          r={R}
+          stroke={STROKE_COLOR}
+          strokeWidth={15}
+          strokeDasharray={CIRCLE_LENGTH}
+          animatedProps={animatedProps}
+          strokeLinecap={"round"}
+        />
+      </Svg>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>Run</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    bottom: 80,
+    width: width * 0.7,
+    height: 60,
+    backgroundColor: BACKGROUND_STROKE_COLOR,
+    position: "absolute",
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 25,
+    color: "white",
+    letterSpacing: 2.0,
+  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: BACKGROUND_COLOR,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  progressText: {
+    fontSize: 80,
+    color: "rgba(256, 256, 256, 0.7)",
+    width: 200,
+    textAlign: "center",
   },
 });
