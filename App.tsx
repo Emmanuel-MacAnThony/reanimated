7cb8262712ba68ff3bf4ca5bf9401dd678bc57d7
emmadev <macanthonyemmanuel12@gmail.com>
2023-09-19 19:57:45 +0100

    animates flatlist

--- App.tsx
+++ App.tsx
@@ -1,114 +1,40 @@
 import {
+  FlatList,
+  StatusBar,
   StyleSheet,
   Text,
   View,
-  ScrollView,
-  SafeAreaView,
-  StatusBar,
-  TouchableOpacity,
+  ViewToken,
 } from "react-native";
-import React, { useState, useCallback, useRef, useEffect } from "react";
-import Animated, {
-  FadeIn,
-  FadeOut,
-  Layout,
-  ZoomIn,
-  ZoomOut,
-} from "react-native-reanimated";
+import React from "react";
+import ListItem from "./components/ListItem";
+import { useSharedValue } from "react-native-reanimated";
 
-export const LIST_ITEM_COLOR = "#1798DE";
+const data = new Array(50).fill(0).map((_, index) => ({ id: index }));
 
-interface Item {
-  id: number;
-}
-
-// new Array(5).fill(0).map((_, idx) => { return { id: idx };})
-
-const App = () => {
-  const [items, setItems] = useState<Item[]>(
-    new Array(5).fill(0).map((_, idx) => {
-      return { id: idx };
-    })
-  );
-
-  const initialMode = useRef<boolean>(true);
-
-  useEffect(() => {
-    initialMode.current = false;
-  }, []);
-
-  const onAdd = useCallback(() => {
-    setItems((currentItems) => {
-      const nextItemId = (currentItems[currentItems.length - 1]?.id ?? 0) + 1;
-      return [...currentItems, { id: nextItemId }];
-    });
-  }, []);
-
-  const onDelete = useCallback((itemId: number) => {
-    setItems((currentItems) => {
-      return currentItems.filter((item) => item.id !== itemId);
-    });
-  }, []);
-
+export default function App() {
+  const viewableItems = useSharedValue<ViewToken[]>([]);
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar backgroundColor={"transparent"} />
-      <TouchableOpacity style={styles.floatingButton} onPress={onAdd}>
-        <Text style={{ color: "white", fontSize: 40 }}>+</Text>
-      </TouchableOpacity>
-      <ScrollView
-        style={{ flex: 1 }}
-        contentContainerStyle={{ paddingVertical: 50 }}
-      >
-        {items.map((item, idx) => {
-          return (
-            <Animated.View
-              entering={initialMode.current ? FadeIn.delay(100 * idx) : FadeIn}
-              exiting={FadeOut}
-              style={styles.listItem}
-              key={item.id}
-              onTouchEnd={() => onDelete(item.id)}
-              layout={Layout.delay(100)}
-            />
-          );
-        })}
-      </ScrollView>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <StatusBar />
+      <FlatList
+        contentContainerStyle={{ paddingTop: 40 }}
+        data={data}
+        renderItem={({ item }) => {
+          return <ListItem item={item} viewableItems={viewableItems} />;
+        }}
+        keyExtractor={(item) => item.id.toString()}
+        onViewableItemsChanged={({ viewableItems: vItems }) => {
+          viewableItems.value = vItems;
+        }}
+      />
+    </View>
   );
-};
-
-export default App;
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    //alignItems: "center",
-    //justifyContent: "center",
-  },
-  floatingButton: {
-    width: 80,
-    aspectRatio: 1,
-    backgroundColor: "black",
-    borderRadius: 40,
-    position: "absolute",
-    bottom: 50,
-    right: "5%",
-    zIndex: 10,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  listItem: {
-    height: 100,
-    backgroundColor: LIST_ITEM_COLOR,
-    width: "90%",
-    marginVertical: 10,
-    borderRadius: 20,
-    alignSelf: "center",
-    elevation: 5,
-    shadowColor: "black",
-    shadowOpacity: 0.15,
-    shadowOffset: { width: 0, height: 10 },
-    shadowRadius: 20,
   },
 });
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,139 +1,41 @@
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ViewToken } from "react-native";
 import React from "react";
-import { TaskInterface } from "../App";
-import {
-  PanGestureHandler,
-  PanGestureHandlerGestureEvent,
-  PanGestureHandlerProps,
-} from "react-native-gesture-handler";
 import Animated, {
-  runOnJS,
-  useAnimatedGestureHandler,
   useAnimatedStyle,
-  useSharedValue,
   withTiming,
 } from "react-native-reanimated";
-import { FontAwesome5 } from "@expo/vector-icons";
 
-interface ListItemProps
-  extends Pick<PanGestureHandlerProps, "simultaneousHandlers"> {
-  task: TaskInterface;
-  onDismiss?: (task: TaskInterface) => void;
+interface IProps {
+  viewableItems: Animated.SharedValue<ViewToken[]>;
+  item: {
+    id: number;
+  };
 }
 
-const { width: SCREEN_WIDTH } = Dimensions.get("window");
-
-const LIST_ITEM_HEIGHT = 70;
-
-const TRANSLATE_X_THRESHOLD = -SCREEN_WIDTH * 0.3;
-
-const ListItem: React.FC<ListItemProps> = ({
-  task,
-  onDismiss,
-  simultaneousHandlers,
-}) => {
-  const translateX = useSharedValue(0);
-  const itemHeight = useSharedValue(LIST_ITEM_HEIGHT);
-  const marginVertical = useSharedValue(10);
-  const opacity = useSharedValue(1);
-
-  const panGestureEventHandler =
-    useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
-      onStart: () => {},
-      onActive: (event) => {
-        translateX.value = event.translationX;
-      },
-      onEnd: () => {
-        const shouldBeDismissed = translateX.value < TRANSLATE_X_THRESHOLD;
-        if (shouldBeDismissed) {
-          translateX.value = withTiming(-SCREEN_WIDTH);
-          itemHeight.value = withTiming(0);
-          marginVertical.value = withTiming(0);
-          opacity.value = withTiming(0, undefined, (isFinished) => {
-            if (isFinished && onDismiss) {
-              runOnJS(onDismiss)(task);
-            }
-          });
-        } else {
-          translateX.value = withTiming(0);
-        }
-      },
-    });
-
+const ListItem: React.FC<IProps> = ({ viewableItems, item }) => {
   const rStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateX: translateX.value }],
-    };
-  });
-
-  const rIconContainerStyle = useAnimatedStyle(() => {
-    const opacity = withTiming(
-      translateX.value < TRANSLATE_X_THRESHOLD ? 1 : 0
+    const isVisible = Boolean(
+      viewableItems.value
+        .filter((item) => item.isViewable)
+        .find((viewableItem) => viewableItem.item.id === item.id)
     );
-    return { opacity };
-  });
-
-  const rTaskContainerStyle = useAnimatedStyle(() => {
     return {
-      height: itemHeight.value,
-      marginVertical: marginVertical.value,
-      opacity: opacity.value,
+      opacity: withTiming(isVisible ? 1 : 0),
+      transform: [{ scale: withTiming(isVisible ? 1 : 0.6) }],
     };
   });
 
-  return (
-    <Animated.View style={[styles.taskContainer, rTaskContainerStyle]}>
-      <Animated.View style={[styles.iconContainer, rIconContainerStyle]}>
-        <FontAwesome5
-          name={"trash-alt"}
-          size={LIST_ITEM_HEIGHT * 0.4}
-          color="red"
-        />
-      </Animated.View>
-      <PanGestureHandler
-        onGestureEvent={panGestureEventHandler}
-        simultaneousHandlers={simultaneousHandlers}
-      >
-        <Animated.View style={[styles.task, rStyle]}>
-          <Text style={styles.taskTitle}>{task.title}</Text>
-        </Animated.View>
-      </PanGestureHandler>
-    </Animated.View>
-  );
+  return <Animated.View style={[styles.listItem, rStyle]} />;
 };
 
 const styles = StyleSheet.create({
-  iconContainer: {
-    height: LIST_ITEM_HEIGHT,
-    width: LIST_ITEM_HEIGHT,
-    position: "absolute",
-    right: "10%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  task: {
+  listItem: {
+    height: 80,
     width: "90%",
-    height: LIST_ITEM_HEIGHT,
-    backgroundColor: "white",
-    justifyContent: "center",
-    paddingLeft: 20,
-    shadowOpacity: 0.08,
-    shadowOffset: {
-      width: 0,
-      height: 20,
-    },
-    shadowRadius: 10,
-    elevation: 5,
-    borderRadius: 10,
-  },
-
-  taskContainer: {
-    width: "100%",
-    alignItems: "center",
-  },
-
-  taskTitle: {
-    fontSize: 16,
+    backgroundColor: "#78CAD2",
+    marginTop: 20,
+    borderRadius: 15,
+    alignSelf: "center",
   },
 });
 
